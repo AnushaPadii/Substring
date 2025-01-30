106328596585faa1d9c706aede004f66c5f0e747
AnushaPadii <anushapadi142@gmail.com>
2025-01-30 22:08:58 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str="How are you?";
+        String substring=str.substring(3);
+        System.out.println("Substring:"+substring);
+    }
+    
+}
